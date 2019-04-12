49d1d8bfd9b9177301c89c1bf99aa0585e2eb671
moshe malawach <moshe.malawach@protonmail.com>
2019-04-12 14:26:52 +0200

    babel polyfill

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -49,7 +49,10 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client'), resolve('aleph-js')],
+        options: {
+          presets: ['env']
+        }
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+require("babel-polyfill");
 import Vue from 'vue'
 import BootstrapVue from "bootstrap-vue"
 import App from './App'
